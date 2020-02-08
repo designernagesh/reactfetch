2a6b4605a51c83da01d9eb73b44073ef27c4b8a9
nagesh <designer.nagesh@gmail.com>
2020-02-08 12:07:43 +0530

    fetch api project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {      
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/todos") // It is a Promise
+    .then(result => result.json()) // It is a Promise
+    .then(json => {
+      console.log(json)
+      this.setState({
+        isLoaded: true,
+        items:json
+      })      
+    })
+  }
+
+  render(){
+    var {isLoaded, items} = this.state;
+    if(!isLoaded){
+      return <h2>No Data Found...</h2>
+    }
+    else{
+      return (
+        <div className="App">
+          <h1> The Employee Data</h1>
+
+          <table border="1">
+          <tr><th>ID</th><th>Title</th></tr>
+
+          <tbody>
+            {items.map(item =>(
+              
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+              </tr>
+              ))}
+          </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
