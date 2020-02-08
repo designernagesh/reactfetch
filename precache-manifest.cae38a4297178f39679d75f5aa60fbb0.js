fc80b8cee1f47404451ea339b29db3df5eab03dc
nagesh <designer.nagesh@gmail.com>
2020-02-08 12:16:32 +0530

    Updates

---
+++ precache-manifest.cae38a4297178f39679d75f5aa60fbb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1e6cb44090b265ed366fab2d2687f5",
+    "url": "/reactfetch/index.html"
+  },
+  {
+    "revision": "a4d6b0ea6608b5d74f5c",
+    "url": "/reactfetch/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8185cec8c5b02cfda057",
+    "url": "/reactfetch/static/js/2.8b0b091b.chunk.js"
+  },
+  {
+    "revision": "a4d6b0ea6608b5d74f5c",
+    "url": "/reactfetch/static/js/main.feaa7a38.chunk.js"
+  },
+  {
+    "revision": "9789b369b7c1436cadcb",
+    "url": "/reactfetch/static/js/runtime-main.dd0e2f70.js"
+  }
+]);
